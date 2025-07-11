corrected demodulation.py units + intervals of integration

Demodulation.py
@@ -16,13 +16,13 @@ def cos2weight(time):
 
 #this function is used for integration to demodulate and extract phase & amplitude
 def cosDemod(time, params): #requires params argument even if unused since integration
-    omega = (2 * np.pi) / (3600*23 + 56*60 + 4) #angular frequency of sidereal day
+    omega = (2 * np.pi) / (1000*(3600*23 + 56*60 + 4)) #angular frequency of sidereal day
     time1 = time[0]
     phase = 0
     return np.cos(2 * omega * (time1 - phase))
 
 def sinDemod(time, params):
-    omega = (2 * np.pi) / (3600*23 + 56*60 + 4) #angular frequency of sidereal day
+    omega = (2 * np.pi) / (1000*(3600*23 + 56*60 + 4)) #angular frequency of sidereal day
     time1 = time[0]
     phase = 0
     return np.sin(2 * omega * (time1 - phase))
@@ -51,17 +51,16 @@ if __name__ == "__main__":
 
     print("Number of events:", len(timeArr))
 
-    # Plotting the energy distribution with sine squared weighting
-    # energyScatter = plt.scatter(timeArr, energyArr, c=weightArr, cmap='viridis', s=1)
-    # plt.colorbar(energyScatter, label='Sine Squared Weighting')
-    # plt.xlabel('Event Time (s)')
-    # plt.ylabel('Energy (keV)')
-    # plt.ylim(1280, 1530)
-
     E_upper = 1530
     E_lower = 1280
     T_lower = np.min(timeArr)
-    T_upper = np.max(timeArr)
+
+    timeArr = timeArr - T_lower # normalize time to start at 0
+    T_upper = np.max(timeArr) # now T_upper is relative to T_lower
+    T_lower = 0 # now T_lower is 0
+    sidereal_day = (3600*23 + 56*60 + 4)*1000 #milliseconds in a sidereal day
+    
+
     eRes = 1 # 1 keV resolution
     tRes = 1000*3600*3 # hr resolution in milliseconds
 
@@ -69,22 +68,23 @@ if __name__ == "__main__":
     eBins = int((E_upper - E_lower)/eRes)
     timeBins = int((T_upper - T_lower)/tRes)
     global EvT_Hist
-    EvT_Hist = ROOT.TH2D("EvT_Hist", "Energy vs Time;Event Time/3h (ms);Energy/KeV (KeV)", timeBins, T_lower, T_upper, eBins, E_lower, E_upper)
+    EvT_Hist = ROOT.TH2D("EvT_Hist", "Energy vs Time;Elapsed Time/3h (ms);Energy/KeV (KeV)", timeBins, T_lower, T_upper, eBins, E_lower, E_upper)
     for i in range(len(timeArr)):
         EvT_Hist.Fill(timeArr[i], energyArr[i], weightArr[i])
-    # c1 = ROOT.TCanvas("c1", "Energy vs Time", 800, 600)
-    # c1.Draw()
+    c1 = ROOT.TCanvas("c1", "Energy vs Time", 800, 600)
+    c1.Draw()
     EvT_Hist.Draw("Lego")
-    # c1.SaveAs("Energy_vs_Time.png")
+    EvT_Hist.GetYaxis().SetRangeUser(1450,1470) #set y-axis range
 
+    c2 = ROOT.TCanvas("TC", "Energy vs Time", 800, 600)
     etScatter = ROOT.TGraph2D(len(timeArr), np.float64(timeArr), np.float64(energyArr), np.float64(weightArr))
-
     etScatter.GetYaxis().SetRangeUser(1280, 1530)
     etScatter.GetXaxis().SetTitleOffset(2.5)
     etScatter.GetYaxis().SetTitleOffset(2)
-    etScatter.SetTitle("Energy Weighted vs Time (Summer 2020);Unix Time (ms);Energy (keV);Sin Squared Weighting")
+    etScatter.SetTitle("Energy Weighted vs Time (Summer 2020);Elapsed Time (ms);Energy (keV);Sin Squared Weighting")
     etScatter.SetMarkerStyle(8) #changes from default square to large dot
     etScatter.SetMarkerSize(0.4)
+    c2.Draw()
     etScatter.Draw("PCOL0") # plot markers with color, no stats box
 
     etScatter.GetYaxis().SetRangeUser(1459, 1461) #redefine to narrow gap instead
@@ -93,15 +93,18 @@ if __name__ == "__main__":
     validEnergies = validEnergies[(1459. <= validEnergies) & (validEnergies <= 1461.)] # filter energies in range [1459, 1461]
     nEvents = validEnergies.size
     
-    inPhase = ROOT.TF1("f1", cosDemod, timeArr[0], timeArr[-1]); #create a TF1 object for cosine demodulation
-    quadrature = ROOT.TF1("f2", sinDemod, timeArr[0], timeArr[-1]); #create a TF1 object for sine demodulation
+    inPhase = ROOT.TF1("f1", cosDemod, timeArr[0], timeArr[-1]) #create a TF1 object for cosine demodulation
+    quadrature = ROOT.TF1("f2", sinDemod, timeArr[0], timeArr[-1]) #create a TF1 object for sine demodulation
 
-    err = ctypes.c_double(0.0)
-    integ = inPhase.IntegralOneDim(timeArr[0], timeArr[-1],1e-3,1e-3,err) #create a TF1 object for cosine demodulation, 1.E-3, 1.E-3, *err); //1.E-3 is relaxed tolerance
-    integ2 = quadrature.IntegralOneDim(timeArr[0], timeArr[-1],1e-3,1e-3,err) # create a TF1 object for cosine demodulation, 1.E-3, 1.E-3, *err);
-    iqPhase = ROOT.TMath.ATan2(nEvents * integ2, nEvents * integ)
+    err = ctypes.c_double(5)
+
+    periods = timeArr % sidereal_day # get the periods of the sidereal day
+    ##should come up with more robust way to find a period, but this works for now
+    inPhaseIntegral = inPhase.IntegralOneDim(timeArr[periods.argmin()], timeArr[periods.argmax()],1e-6,1e-6,err)
+    quadIntegral = quadrature.IntegralOneDim(timeArr[periods.argmax()], timeArr[periods.argmax()],1e-6,1e-6,err)
+    iqPhase = ROOT.TMath.ATan2(nEvents * quadIntegral, nEvents * inPhaseIntegral)
     #arctan will cancel out constants leaving only phase
-    amplitude = ROOT.TMath.Hypot(nEvents * integ, nEvents * integ2)
+    amplitude = ROOT.TMath.Hypot(nEvents * inPhaseIntegral, nEvents * quadIntegral)
 
     print("\n\nThe total number of events in Jul-Aug 2020 dataset is: ", total)
     print("The number of events in the energy range [1459, 1461] is: ", nEvents)