9cf686c20b5518f1a631c56443a5176300a8e812
zakronlage <zakronlage@gmail.com>
2025-07-17 02:12:09 -0700

    Add files via upload
    with while loop method

--- Demodulation.C
+++ Demodulation.C
@@ -8,6 +8,7 @@
 #include <TF1.h>
 #include <ROOT/RVec.hxx>
 #include <TGraph2D.h>
+#include <numeric>
 
 //this function is used for weighting based on time
 double sin2weight(ULong64_t time) {
@@ -53,60 +54,82 @@ void Demodulation() {
     auto timeArr = withSin2Weights2.Take<ULong64_t>("EventTime");
     auto energyArr = withSin2Weights2.Take<double>("Energy");
     auto weightArr = withSin2Weights2.Take<double>("Sin2Weighting");
+    double startTime = (*timeArr)[0];
+    double energyLow = 1300;
+    double energyHigh = 1500;
+    int total = timeArr->size();
 
     //weighting by sin squared function
-    auto g2d = new TGraph2D(timeArr->size());
-    for (int i = 0; i < timeArr->size(); i++) {
-        g2d->SetPoint(i, (*timeArr)[i], (*energyArr)[i], (*weightArr)[i]);
-    }
-    g2d->GetYaxis()->SetRangeUser(1280, 1530);
-    g2d->GetXaxis()->SetTitleOffset(2.5);
-    g2d->GetYaxis()->SetTitleOffset(2);
-    g2d->SetTitle("Energy Weighted vs Time (Summer 2020);Unix Time (ms);Energy (keV);Sin Squared Weighting");
-    g2d->SetMarkerStyle(8); //changes from default square to large dot
-    g2d->SetMarkerSize(0.4);
-    g2d->Draw("PCOL0"); // plot markers with color, no stats box
-
-    c->SaveAs("energyScatter.pdf");
-
-
-    //use integral to extract phase and amp
-    g2d->GetYaxis()->SetRangeUser(1459, 1461); //redefine to narrow gap instead
-    int total = timeArr->size();
-    int nEvents = 0;
-    double *validEnergies = g2d->GetY();
-    //counts number of events
-    for (int i = 0; i < g2d->GetN(); ++i) {
-        if (validEnergies[i] >= 1459 && validEnergies[i] <= 1461) {
-            nEvents++;
+    auto g2d = new TGraph2D(total);
+    for (int i = 0; i < total; i++) {
+        if (energyLow < (*energyArr)[i] && (*energyArr)[i] < energyHigh) {
+            //Take method returns pointer, so dereference first
+            double fixTime = ((*timeArr)[i] - startTime) / 1000;
+            g2d->SetPoint(i, fixTime, (*energyArr)[i], (*weightArr)[i]);
         }
     }
-    auto inPhase = new TF1("f1", cosDemod, (*timeArr)[0], (*timeArr)[timeArr->size() - 1]);
-    auto quadrature = new TF1("f2", sinDemod, (*timeArr)[0], (*timeArr)[timeArr->size() - 1]);
+    g2d->Draw("PCOL0"); // plot markers with color, no stats box
+    g2d->GetYaxis()->SetRangeUser(1320, 1480); 
+    //slightly less than energyLow and High to avoid fXmax error
+    g2d->GetXaxis()->SetTitleOffset(2.5);
+    g2d->GetYaxis()->SetTitleOffset(2);
+    g2d->SetTitle("Energy Weighted vs Time (Summer 2020);Unix Time (s);Energy (keV);Sin Squared Weighting");
+    g2d->SetMarkerStyle(8); //changes from default square to large dot
+    g2d->SetMarkerSize(0.4);
 
-    Double_t allowedErr = 0.;
-    Double_t *err = &allowedErr;
-    auto integ = inPhase->IntegralOneDim((*timeArr)[0], (*timeArr)[timeArr->size() - 1], 1.E-3, 1.E-3, *err); //1.E-3 is relaxed tolerance
-    auto integ2 = quadrature->IntegralOneDim((*timeArr)[0], (*timeArr)[timeArr->size() - 1], 1.E-3, 1.E-3, *err);
-    double iqPhase = TMath::ATan2(nEvents * integ2, nEvents * integ); 
+    c->Update();
+    c->SaveAs("energyScatter.pdf");
+
+    //extract phase and amp
+    std::vector<int> eventArr; //dynamically sized std::vector arrays
+    std::vector<int> nrgArr;
+    std::vector<double> inPhaseArr;
+    std::vector<double> quadArr;
+    int siderealDay = 3600*23 + 56*60 + 4;
+    int currTime = 0;
+    int endTime = 0;
+
+    while (endTime < timeArr->size()) {
+        endTime += siderealDay;
+        int nEvents = 0;
+        std::vector<double> nrgAvg;
+        for (int i = currTime; i < endTime; i++) { //figures stuff out for each sidereal period
+            if (1459 <= (*energyArr)[i] && (*energyArr)[i] <= 1461) {
+                nEvents++;
+                nrgAvg.push_back((*energyArr)[i]); //adds it to dynamic sized array
+            }
+        }
+        eventArr.push_back(nEvents);
+
+        int avgNrg = std::accumulate(nrgAvg.begin(), nrgAvg.end(), 0);
+        nrgArr.push_back(avgNrg / nrgAvg.size());
+
+        Double_t allowedErr = 0.;
+        Double_t *err = &allowedErr;
+        //creates TF1 object from [0] to [total]
+        auto inPhase = new TF1("f1", cosDemod, (*timeArr)[0], (*timeArr)[total]);
+        double integVal = inPhase->IntegralOneDim(currTime, endTime, 1.E-6, 1.E-3, *err); //1.E-3 is relaxed tolerance;
+        inPhaseArr.push_back(integVal);
+
+        auto quadrature = new TF1("f2", sinDemod, (*timeArr)[0], (*timeArr)[total]);
+        double integVal2 = quadrature->IntegralOneDim(currTime, endTime, 1.E-6, 1.E-3, *err);
+        quadArr.push_back(integVal2);
+
+        currTime = endTime; //move other pointer forward
+    }
+    int totalROI = std::accumulate(eventArr.begin(), eventArr.end(), 0);
+    int totalROIper = totalROI / eventArr.size(); //total per sidereal period
+
+    double integRes = std::accumulate(inPhaseArr.begin(), inPhaseArr.end(), 0);
+    double integRes2 = std::accumulate(quadArr.begin(), quadArr.end(), 0);
+
+    double iqPhase = TMath::ATan2(integRes2, integRes);
     //arctan will cancel out constants leaving only phase
-    double amplitude = TMath::Hypot(nEvents * integ, nEvents * integ2);
+    double amplitude = TMath::Hypot(totalROIper * integRes, totalROIper * integRes2);
 
     std::cout << "\n\nThe total number of events in Jul-Aug 2020 dataset is: " << total << std::endl;
-    std::cout << "The number of events in the energy range [1459, 1461] is: " << nEvents << std::endl;
-    std::cout << "\nThe phase is: " << iqPhase << std::endl;
-    std::cout << "The amplitude is: " << amplitude << "\n" << std::endl;
-
-    
-    //try snapshot to a temporary ROOT file with custom columns
-    auto snapshot = withSin2Weights2.Snapshot("tempTree", "temp_snapshot.root", {"Energy", "EventTime", "Sin2Weighting"});
-    TFile snap("temp_snapshot.root");
-    auto tree = (TTree*)snap.Get("tempTree");
-    //workaround to draw scatterplot with Draw() by not using RDataFrame
-
-    auto d = new TCanvas();
-
-    tree->Draw("Energy:EventTime", "Sin2Weighting"); 
-
-    d->SaveAs("2dscatter.pdf");
+    std::cout << "The total number of events in the energy range [1459, 1461] is: " << totalROI << std::endl;
+    std::cout << "The average number of events in range per sidereal day is: " << totalROIper << std::endl;
+    std::cout << "\nThe average phase is: " << iqPhase << std::endl;
+    std::cout << "The average amplitude is: " << amplitude << "\n\n" << std::endl;
 }
